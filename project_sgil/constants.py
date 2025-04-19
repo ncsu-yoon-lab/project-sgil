e81f27ee05716ba33b4db556599bf44d5c6452d6
jackg <jackgeorgeelia@gmail.com>
2025-04-19 01:46:32 -0400

    Finished restructuring

--- project_sgil/constants.py
+++ project_sgil/constants.py
@@ -5,7 +5,7 @@ ORIGIN = (35.770771, -78.674804)
 EARTH_RADIUS_M = 6378137 # [m]
 
 # Path to the CSV of tree locations in lat, lon
-TREE_LOCATIONS_PATH = "Data\\TreeLocations-Manual.csv"
+TREE_LOCATIONS_PATH = "../Data/TreeLocations-Manual.csv"
 
 # Path to the data logged while traveling
 DATA_LOGGER_PATH = "Data\\PositionDataLogger.csv"
