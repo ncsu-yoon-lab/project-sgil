3f0567ac0543155d03051d7b38709d18a9524441
chmalinc <chmalinc@ncsu.edu>
2025-08-31 08:35:16 -0400

    Get path vector and yaw now outputs Point and Point for the ends of the vector and a float for the yaw detected from the path

--- project_sgil/localization/path_vector_generator.py
+++ project_sgil/localization/path_vector_generator.py
@@ -1,5 +1,5 @@
-"""Manual path vector selection that is capable of producing a path vector from
-an image.
+"""
+Manual path vector selection that is capable of producing a path vector from an image.
 
 file: path_vector.py
 author: Cole Malinchock and Jack Elia
@@ -15,6 +15,7 @@ import pandas as pd
 from OSMPythonTools.overpass import Overpass
 
 from project_sgil.constants import H_FOV_DEG, ORIGIN, V_FOV_DEG
+from project_sgil.data_structs import Point
 
 # Import custom libraries
 from project_sgil.utils.converter import Converter
@@ -43,7 +44,7 @@ class PathVectorGenerator:
         self.points = None
         self.fx = (self.width / 2) / math.tan(math.radians(H_FOV_DEG / 2))
         self.fy = (self.height / 2) / math.tan(math.radians(V_FOV_DEG / 2))
-        self.center_point = (int(self.width / 2), int(self.height / 2))
+        self.center_point = Point(x=self.width / 2, y=self.height / 2)
         self.camera_height = camera_height
         self.dataset = dataset
         self.overpass = Overpass()
@@ -57,7 +58,8 @@ class PathVectorGenerator:
         flags: int,
         param: tuple[list[tuple[int, int]], np.ndarray],
     ) -> None:
-        """Handles the click events.
+        """
+        Handles the click events.
 
         :param event: The click event instance occuring
         :param x: The x location of the event
@@ -71,7 +73,7 @@ class PathVectorGenerator:
 
         # Adds a circle at the location of the click
         if event == cv2.EVENT_LBUTTONDOWN and len(self.points) < 4:
-            self.points.append((x, y))
+            self.points.append(Point(x=x, y=y))
             cv2.circle(img_display, (x, y), 5, (0, 255, 0), -1)  # draw green dot
             cv2.putText(
                 img_display, f"P{len(self.points)}", (x, y), 0, 1.0, (0, 255, 0), thickness=2
@@ -80,68 +82,60 @@ class PathVectorGenerator:
 
     def get_intersection(
         self,
-        left_points: tuple[tuple[int, int], tuple[int, int]],
-        right_points: tuple[tuple[int, int], tuple[int, int]],
-    ) -> tuple[tuple[float, float], float, float]:
-        """Gets the intersection between the left and right points and the
-        corresponding slopes.
+        left_points: tuple[Point, Point],
+        right_points: tuple[Point, Point],
+    ) -> tuple[Point, float, float]:
+        """
+        Gets the intersection between the left and right points and the corresponding slopes.
 
         :param left_points: The left two points of the edge detected
         :param right_points: The right two points of the edge detected
-        :return: The intersection point and the slope of the left and
-            right lines
+        :return: The intersection point and the slope of the left and right lines
         """
 
         # Gets the slope of the left and right points
-        if left_points[0][0] == left_points[1][0]:
+        if left_points[0].x == left_points[1].x:
             m_left = 9999.0
         else:
-            m_left = (left_points[0][1] - left_points[1][1]) / (
-                left_points[0][0] - left_points[1][0]
-            )
-        if right_points[0][0] == right_points[1][0]:
+            m_left = (left_points[0].y - left_points[1].y) / (left_points[0].x - left_points[1].x)
+        if right_points[0].x == right_points[1].x:
             m_right = 9999.0
         else:
-            m_right = (right_points[0][1] - right_points[1][1]) / (
-                right_points[0][0] - right_points[1][0]
+            m_right = (right_points[0].y - right_points[1].y) / (
+                right_points[0].x - right_points[1].x
             )
 
         # Gets the y-intercept of the left and right lines
-        b_left = left_points[0][1] - m_left * left_points[0][0]
-        b_right = right_points[0][1] - m_right * right_points[0][0]
+        b_left = left_points[0].y - m_left * left_points[0].x
+        b_right = right_points[0].y - m_right * right_points[0].x
 
         # Gets the x and y intersection point
         x_int = (b_left - b_right) / (m_right - m_left)
         y_int = m_left * x_int + b_left
 
-        return (x_int, y_int), m_left, m_right
+        return Point(x=x_int, y=y_int), m_left, m_right
 
     def get_displacement_and_yaw(
         self,
-        intersection_point: tuple[float, float],
+        intersection_point: Point,
         m_left: float,
         m_right: float,
         path_width: float,
         path_yaw: float,
     ) -> tuple[float, float]:
-        """Gets the displacement and yaw of the system based on the results of
-        the edges detected.
+        """
+        Gets the displacement and yaw of the system based on the results of the edges detected.
 
-        :param intersection_point: The intersecting point between the
-            two edges
+        :param intersection_point: The intersecting point between the two edges
         :param m_left: The slope of the left line
         :param m_right: The slope of the right line
         :param path_width: The width of the path
         :param path_yaw: The yaw of the path globally
-        :return: The estimated displacement and yaw of the system
-            relative to the path
+        :return: The estimated displacement and yaw of the system relative to the path
         """
 
-        # Gets the center point coordinates
-        cx, cy = self.center_point
-
         # Finds the difference in the x of the center point and intersection
-        dx = intersection_point[0] - cx
+        dx = intersection_point.x - self.center_point.x
 
         # Gets the relative yaw based on the difference in x and the focal in the x
         relative_yaw_deg = math.degrees(math.atan(dx / self.fx))
@@ -153,11 +147,9 @@ class PathVectorGenerator:
 
         return x_delta, global_yaw
 
-    def get_closest_point_along_line(
-        self, p1: tuple[float, float], p2: tuple[float, float], p3: tuple[float, float]
-    ) -> float:
-        """Find the point where p3 intersects perpendicular to the line from p1
-        to p2.
+    def get_closest_point_along_line(self, p1: Point, p2: Point, p3: Point) -> float:
+        """
+        Find the point where p3 intersects perpendicular to the line from p1 to p2.
 
         :param p1: The first point of the line
         :param p2: The second point of the line
@@ -165,43 +157,36 @@ class PathVectorGenerator:
         :return: The distance from p1 to the line from p1 to p2
         """
 
-        # Extract the coordinates of the points
-        x1, y1 = p1
-        x2, y2 = p2
-        x3, y3 = p3
-
         # Get the inverse slope of the line
-        m = 9999 if x1 == x2 else (y2 - y1) / (x2 - x1)
+        m = 9999 if p1.x == p2.x else (p2.y - p1.y) / (p2.x - p1.x)
         m_inv = -1 / m
 
         # Get the inverse y intercept of the line
-        b = y1 - m * x1
-        b_inv = y3 - m_inv * x3
+        b = p1.y - m * p1.x
+        b_inv = p3.y - m_inv * p3.x
 
         # Find the intersection point between the line and p3
         x_int = (b - b_inv) / (m_inv - m)
         y_int = x_int * m + b
 
         # Calculate its distance
-        dist = math.sqrt((x_int - x3) ** 2 + (y_int - y3) ** 2)
+        dist = math.sqrt((x_int - p3.x) ** 2 + (y_int - p3.y) ** 2)
 
         return dist
 
     def get_path_data(
         self,
         estimated_position_latlon: tuple[float, float],
-        estimated_position_xy: tuple[float, float],
+        estimated_position_xy: Point,
         estimated_heading_deg: float,
-    ) -> tuple[tuple[float, float], tuple[float, float], float]:
-        """Gets the data from the path to be handled.
+    ) -> tuple[Point, Point, float, float]:
+        """
+        Gets the data from the path to be handled.
 
-        :param estimated_position_latlon: The estimated lat lon of the
-            system
+        :param estimated_position_latlon: The estimated lat lon of the system
         :param estimated_position_xy: The estimated x y of the system
-        :param estimated_heading_deg: The estimated heading of the
-            system
-        :return: The start and end points of the path and the width of
-            the path
+        :param estimated_heading_deg: The estimated heading of the system
+        :return: The start and end points of the path and the width of the path
         """
 
         # Extracts the lat and lon from the estimation
@@ -222,8 +207,8 @@ class PathVectorGenerator:
                 if previous_node is not None:
                     # Gets the distance from estimated position to the path nodes
 
-                    prev_node_xy = self.converter.latlon_to_xy(previous_node)
-                    cur_node_xy = self.converter.latlon_to_xy(latlon_node)
+                    prev_node_xy = Point(*self.converter.latlon_to_xy(previous_node))
+                    cur_node_xy = Point(*self.converter.latlon_to_xy(latlon_node))
 
                     dist = self.get_closest_point_along_line(
                         prev_node_xy, cur_node_xy, estimated_position_xy
@@ -241,31 +226,31 @@ class PathVectorGenerator:
                     previous_node = latlon_node
 
         # Gets the delta to each node from estimated pose
-        estimated_pose_vector = (
-            math.cos(math.radians(estimated_heading_deg)),
-            math.sin(math.radians(estimated_heading_deg)),
+        estimated_pose_vector = Point(
+            x=math.cos(math.radians(estimated_heading_deg)),
+            y=math.sin(math.radians(estimated_heading_deg)),
         )
-        prev_node_vector = (
-            prev_node_xy[0] - estimated_position_xy[0],
-            prev_node_xy[1] - estimated_position_xy[1],
+        prev_node_vector = Point(
+            x=closest_prev_node_xy.x - estimated_position_xy.x,
+            y=closest_prev_node_xy.y - estimated_position_xy.y,
         )
-        prev_node_vector_dist = math.sqrt(prev_node_vector[0] ** 2 + prev_node_vector[1] ** 2)
-        cur_node_vector = (
-            cur_node_xy[0] - estimated_position_xy[0],
-            cur_node_xy[1] - estimated_position_xy[1],
+        prev_node_vector_dist = math.sqrt(prev_node_vector.x**2 + prev_node_vector.y**2)
+        cur_node_vector = Point(
+            x=closest_cur_node_xy.x - estimated_position_xy.x,
+            y=closest_cur_node_xy.y - estimated_position_xy.y,
         )
-        cur_node_vector_dist = math.sqrt(cur_node_vector[0] ** 2 + cur_node_vector[1] ** 2)
+        cur_node_vector_dist = math.sqrt(cur_node_vector.x**2 + cur_node_vector.y**2)
         delta_heading_prev_node = math.acos(
             (
-                estimated_pose_vector[0] * prev_node_vector[0]
-                + estimated_pose_vector[1] * prev_node_vector[1]
+                estimated_pose_vector.x * prev_node_vector.x
+                + estimated_pose_vector.y * prev_node_vector.y
             )
             / prev_node_vector_dist
         )
         delta_heading_cur_node = math.acos(
             (
-                estimated_pose_vector[0] * cur_node_vector[0]
-                + estimated_pose_vector[1] * cur_node_vector[1]
+                estimated_pose_vector.x * cur_node_vector.x
+                + estimated_pose_vector.y * cur_node_vector.y
             )
             / cur_node_vector_dist
         )
@@ -281,67 +266,66 @@ class PathVectorGenerator:
             end_node = closest_cur_node_xy
 
         # Get the delta in the start and end nodes to get the heading of the path
-        dx = end_node[0] - start_node[0]
-        dy = end_node[1] - start_node[1]
+        dx = end_node.x - start_node.x
+        dy = end_node.y - start_node.y
         closest_path_heading = (90 - math.degrees(math.atan2(dy, dx))) % 360
 
         return start_node, end_node, closest_path_width, closest_path_heading
 
     def get_path_vector(
         self,
-        start_node_xy: tuple[float, float],
-        end_node_xy: tuple[float, float],
+        start_node_xy: Point,
+        end_node_xy: Point,
         displacement: float,
-    ) -> tuple[float, float]:
-        """Gets the vector of the path from the node and displacement.
+    ) -> tuple[Point, Point]:
+        """
+        Gets the vector of the path from the node and displacement.
 
         :param start_node_xy: The starting point of the path
         :param end_node_xy: The ending point of the path
         :param displacement: The displacement on the path
-        :return: The start and end points from the displacement
-            perpendicular to the path
+        :return: The start and end points from the displacement perpendicular to the path
         """
 
         # Creates a unit vector of the path vector
         path_dist = math.sqrt(
-            (end_node_xy[0] - start_node_xy[0]) ** 2 + (end_node_xy[1] - end_node_xy[0]) ** 2
+            (end_node_xy.x - start_node_xy.x) ** 2 + (end_node_xy.y - start_node_xy.y) ** 2
         )
-        path_unit_vector = (
-            (end_node_xy[0] - start_node_xy[0]) / path_dist,
-            (end_node_xy[1] - start_node_xy[1]) / path_dist,
+        path_unit_vector = Point(
+            x=(end_node_xy.x - start_node_xy.x) / path_dist,
+            y=(end_node_xy.y - start_node_xy.y) / path_dist,
         )
 
         # Creates the inverted unit vector of the path based on the direction of the displacement
         if displacement < 0:
-            inv_path_unit_vector = (path_unit_vector[1], -path_unit_vector[0])
+            inv_path_unit_vector = Point(x=path_unit_vector.y, y=-path_unit_vector.x)
         else:
-            inv_path_unit_vector = (-path_unit_vector[1], path_unit_vector[0])
+            inv_path_unit_vector = Point(x=-path_unit_vector.y, y=path_unit_vector.x)
 
         # Transforms the start and end nodes based on the inverted unit vector
         # and the displacement magnitude
 
-        transformed_start_node_xy = (
-            start_node_xy[0] + inv_path_unit_vector[0] * abs(displacement),
-            start_node_xy[1] + inv_path_unit_vector[1] * abs(displacement),
+        transformed_start_node_xy = Point(
+            x=start_node_xy.x + inv_path_unit_vector.x * abs(displacement),
+            y=start_node_xy.y + inv_path_unit_vector.y * abs(displacement),
         )
-        transformed_end_node_xy = (
-            end_node_xy[0] + inv_path_unit_vector[0] * abs(displacement),
-            end_node_xy[1] + inv_path_unit_vector[1] * abs(displacement),
+        transformed_end_node_xy = Point(
+            x=end_node_xy.x + inv_path_unit_vector.x * abs(displacement),
+            y=end_node_xy.y + inv_path_unit_vector.y * abs(displacement),
         )
 
         return (transformed_start_node_xy, transformed_end_node_xy)
 
     def get_path_vector_and_yaw(
         self, image: np.ndarray, index: int
-    ) -> tuple[tuple[tuple[float, float], tuple[float, float]], float]:
-        """Gets the path vector and estimated yaw from the image after manually
-        selecting the edges of the path and given an estimated location.
+    ) -> tuple[tuple[Point, Point], float]:
+        """
+        Gets the path vector and estimated yaw from the image after manually selecting the edges of
+        the path and given an estimated location.
 
         :param image: The image being passed through the system
-        :param index: The index of the dataset that the system is
-            currently on
-        :return: The two points that forms a line which the system
-            likely lies on
+        :param index: The index of the dataset that the system is currently on
+        :return: The two points that forms a line which the system likely lies on
         """
 
         # Get the estimated position of the image and convert it to xy
@@ -349,7 +333,7 @@ class PathVectorGenerator:
             self.dataset.rtk_lat.iloc[index],
             self.dataset.rtk_lon.iloc[index],
         )
-        estimated_location_xy = self.converter.latlon_to_xy(estimated_location_latlon)
+        estimated_location_xy = Point(*self.converter.latlon_to_xy(estimated_location_latlon))
         estimated_yaw_deg = self.converter.heading_to_yaw(self.dataset.rtk_heading.iloc[index])
 
         # Make a copy of the image to mark on and initialize the points to be put on the image
@@ -389,9 +373,6 @@ class PathVectorGenerator:
                     # Get the intersection between the 4 points which make 2 lines
                     intersection_point, m_left, m_right = self.get_intersection((p1, p2), (p3, p4))
 
-                    # Round the intersection point for when it is displayed
-                    (int(intersection_point[0]), int(intersection_point[1]))
-
                     # Gets the lateral displacement on the path and the yaw on the path
                     displacement, yaw = self.get_displacement_and_yaw(
                         intersection_point, m_left, m_right, path_width, path_yaw
@@ -414,13 +395,13 @@ class PathVectorGenerator:
                 print(f"Yaw: {yaw} deg")
                 print(
                     f"""Original Path Vector: 
-                    <{end_node_xy[0] - start_node_xy[0]}, 
-                    {end_node_xy[1] - start_node_xy[1]}>"""
+                    <{end_node_xy.x - start_node_xy.x}, 
+                    {end_node_xy.y - start_node_xy.y}>"""
                 )
                 print(
                     f"""Transformed Path Vector: 
-                    <({transformed_path_vector[0][0]}, {transformed_path_vector[0][1]}), 
-                    ({transformed_path_vector[1][0]}, {transformed_path_vector[1][1]})>"""
+                    <({transformed_path_vector[0].x}, {transformed_path_vector[0].y}), 
+                    ({transformed_path_vector[1].x}, {transformed_path_vector[1].y})>"""
                 )
 
                 # Destroys all the windows created by cv
--- project_sgil/manual_sgil.py
+++ project_sgil/manual_sgil.py
@@ -1,5 +1,6 @@
-"""Manual tree selection interface for ground view analysis and matching with
-satellite data using pre-collected and pre-labeled dataset.
+"""
+Manual tree selection interface for ground view analysis and matching with satellite data using pre-
+collected and pre-labeled dataset.
 
 file: manual_selector.py
 author: Cole Malinchock and Jack Elia
@@ -85,7 +86,8 @@ class ManualSGIL:
         self._current_index: int = 0
 
     def get_current_pose(self, image_name: str) -> Pose2d | None:
-        """Lookup the RTK/GPS pose for a given image filename.
+        """
+        Lookup the RTK/GPS pose for a given image filename.
 
         :param image_name: Name of the .jpg image.
         :return: Pose2d if RTK heading is valid; otherwise None.
@@ -98,11 +100,11 @@ class ManualSGIL:
         return self.get_gps_pose(matches.iloc[0])
 
     def get_gps_pose(self, row: pd.Series) -> Pose2d:
-        """Convert a log row into a Pose2d using RTK for yaw.
+        """
+        Convert a log row into a Pose2d using RTK for yaw.
 
         Also updates internal gps_pose/correct_pose for error logging.
-        :param row: pandas Series with rtk_lat, rtk_lon, rtk_heading,
-            gps_lat, gps_lon.
+        :param row: pandas Series with rtk_lat, rtk_lon, rtk_heading, gps_lat, gps_lon.
         :return: Pose2d in local XY + yaw degrees.
         """
         # Convert lat/lon to XY
@@ -118,8 +120,8 @@ class ManualSGIL:
     def get_next_image(
         self,
     ) -> tuple[str, list[Point], Pose2d | None]:
-        """Retrieves the next image, shows it for manual tree picking, and
-        returns the clicks and pose.
+        """
+        Retrieves the next image, shows it for manual tree picking, and returns the clicks and pose.
 
         :return:
           - image_name: filename or "0" when exhausted
@@ -152,14 +154,16 @@ class ManualSGIL:
             return self.get_next_image()
 
         # TODO: use this code eventually, don't delete it
-        # # Performs the vectorization of the path detection
+        # Performs the vectorization of the path detection
         # path_vector, path_yaw = self.path_vector_generator.get_path_vector_and_yaw(
         #     image, self._current_index
         # )
 
         # Define the mouse callback function
         def click_event(event: int, x: int, y: int, flags: list, param: any) -> None:
-            """Handle mouse click events for point selection."""
+            """
+            Handle mouse click events for point selection.
+            """
             # Check if left mouse button was clicked
             if event == cv2.EVENT_LBUTTONDOWN:
                 # Add point to list
