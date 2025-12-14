049d967ac111641250c8ab27001447f883f1cc76
jgelia <jgelia@ncsu.edu>
2025-12-13 23:03:55 -0500

    yay tree detection

---
+++ deep_forest_detection.py
@@ -0,0 +1,12 @@
+from deepforest import main
+from deepforest import get_data
+from deepforest.visualize import plot_results
+# Initialize the model class
+model = main.deepforest()
+
+# Load a pretrained tree detection model from Hugging Face
+model.load_model(model_name="weecology/deepforest-tree", revision="main")
+
+# sample_image_path = get_data("OSBS_029.png")
+img = model.predict_image(path="dataset/satellite_images/fayetteville_st.png")
+plot_results(img)
